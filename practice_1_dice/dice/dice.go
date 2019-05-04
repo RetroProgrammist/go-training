77cad0cbcf41c7b0bf17d7fd0e53c9295b57a2be
Aleksei <jobsprofileru@gmail.com>
2019-05-04 17:01:16 +0300

    does not have time to change the result

--- practice_1_dice/dice/dice.go
+++ practice_1_dice/dice/dice.go
@@ -7,26 +7,48 @@ import (
 
 type Dice struct {
 	faces int
+	lastRandomNum int
 }
 
-/**
+/*
 * It returns 'obj' type dice. 
-**/
+*/
 func Init(faces int) Dice {
 	return Dice {faces: faces}
 }
 
-/**
-* It returns an array with the results of dice throws for 2 dices
-**/
-func (d Dice) RollNTimes(throws int) ([]int, []int) { //фигня с рандомом, будто кэшируется результаты
-	var resultFirstDice, resultSecondDice []int 
+/* 
+* It must return random number (with most entropia)
+* But we have the same value.
+*/
+func (d Dice) Roll() int { //Is some Cache?
+	r := rand.New(rand.NewSource(time.Now().UnixNano())) //Mb need clear variable?
+	return r.Intn(d.faces)+1
+}
+
+/*
+* And here also
+*/
+func (d Dice) RollNTimes(throws int) []int { //Is some Cache?
+	var result []int 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i:= 0; i<throws; i++ {
-		resultFirstDice = append(resultFirstDice, r.Intn(d.faces)+1)
-		resultSecondDice = append(resultSecondDice, r.Intn(d.faces)+1)
+		result = append(result, r.Intn(d.faces)+1)
 	}
-	return resultFirstDice, resultSecondDice
+	return result
+}
+
+/*
+* Another way
+*/
+func (d Dice) hideRoll() int { 
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Intn(d.faces)+1
+}
+
+/*Interface*/
+func GetRandomNum(d Dice) int {
+	return d.hideRoll()
 }
 
 func (d Dice) GetFaces() int{
--- practice_1_dice/main.go
+++ practice_1_dice/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"dice/dice"
-	"dice/stats"
 	"dice/input"
 	"dice/output"
+	"fmt"
 )
 
-const Throws int = 10000
+const Throws int = 10
 
 func main() {	
-	diceObj := dice.Init(input.GetNumFacesFromConsole())
-	result := stats.GetStatistic(diceObj, Throws)
-	output.PrintRow(result.GetSum())
-	output.PrintRow(result.GetRepetitions())
-	output.PrintRow(result.GetPercent())
+	diceObj := dice.Init(input.GetNumFacesFromConsole()) //input 1000
+
+	output.PrintRow(diceObj.RollNTimes(Throws)) //They will be different
+	output.PrintRow(diceObj.RollNTimes(Throws)) //They will be different
+	output.PrintRow(diceObj.RollNTimes(Throws+5)) //They will be different, but first 10 values are equal to the previous arrays
+	for i:=0; i<Throws; i++ {
+		fmt.Printf("%v ", diceObj.Roll())
+	}
+	fmt.Print("\n")
+	for i:=0; i<Throws; i++ {
+		fmt.Printf("%v ", dice.GetRandomNum(diceObj)) 
+	}
 }
